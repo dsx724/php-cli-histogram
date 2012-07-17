d16c93f0aa5db3dc0c2da3034296af9d5d7e5378
Da Xue <dx@ubuntu.(none)>
2012-07-17 12:33:09 -0400

    STDEV Algorithm
    Signed-off-by: Da Xue <dx@ubuntu.(none)>

--- histogram.php
+++ histogram.php
@@ -1,19 +1,29 @@
 <?php
-$array = array(0,1,2,3,4,5,6,7,8,9);
+//Based on http://www.cs.berkeley.edu/~mhoemmen/cs194/Tutorials/variance.pdf
+//iostat -dx 1 /dev/sda | gawk '/sda/ {print $14}' | php histogram.php
 
-$array = array(0,1,2,3,4);
+$stdin = fopen('php://stdin','r');
+if ($stdin === false) die('Unable to open STDIN'.PHP_EOL);
 
-$Ex = 0;
-$Ex2 = 0;
-$n = 0;
-foreach ($array as $input){
-	$Ex += $input;
-	$Ex2 += $input*$input;
-	$n++;
+$input = rtrim(fgets($stdin));
+if ($input === false || !is_numeric($input)) die('Invalid Input:'.$input.PHP_EOL);
+
+$fv = floatval($input);
+$Mk = $fv;
+$Qk = 0;
+$k = 1;
+$t = $fv;
+while (($input = fgets($stdin)) !== false || !is_numeric($input)){
+	$fv = floatval($input);
+	$t += $fv;
+	$diff = $fv - $Mk;
+	$Qk += ($k++) * ($diff) * ($diff) / $k;
+	$Mk += $diff / $k;
+	
+	echo 'Mean:'.number_format($t/$k,2).PHP_EOL;
+	echo 'Var:'.number_format($Qk/($k-1),2).PHP_EOL;
+	echo 'STD:'.number_format(sqrt($Qk/($k-1)),2).PHP_EOL;
 }
-echo $Ex.PHP_EOL.$Ex2.PHP_EOL.$n.PHP_EOL;
-$m = $Ex/$n;
-$std = sqrt(abs(($Ex2 - 2*$Ex*$m + $m*$m)/($n-1)));
-echo 'Mean:'.number_format($m,2).PHP_EOL;
-echo 'STD:'.number_format($std,2).PHP_EOL;
+//time_sleep_until
+//pcntl_fork
 ?>
